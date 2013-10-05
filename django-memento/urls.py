0d2d27b9c86a35d992c9c277ef12606d86f88c49
Dhiraj Thakur <dhirajthakur92@facebook.com>
2013-10-05 21:16:45 +0530

    first page looks nice

--- django-memento/urls.py
+++ django-memento/urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
+from django.views.generic.base import TemplateView
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -12,4 +14,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$',TemplateView.as_view(template_name='index.html'), name='home'),
+
+
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
